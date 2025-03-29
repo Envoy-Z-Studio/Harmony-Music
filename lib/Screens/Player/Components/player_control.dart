5f3d2d7f657de0a4797f27286c06808944c58688
Ꮛnѵoყ-Z <envoyzlab@gmail.com>
2025-03-30 00:30:12 +0530

    Update player_control.dart

--- lib/Screens/Player/Components/player_control.dart
+++ lib/Screens/Player/Components/player_control.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 
 import 'dart:ui';
 
-import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:get/get.dart';
 import 'package:widget_marquee/widget_marquee.dart';
 
@@ -22,118 +21,160 @@ class PlayerControlWidget extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        // Top Section: Song Title, Artist, Favorite & More Button
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: ShaderMask(
-                shaderCallback: (rect) {
-                  return const LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: [Colors.white, Colors.white, Colors.transparent],
-                  ).createShader(Rect.fromLTWH(0, 0, rect.width, rect.height));
-                },
-                blendMode: BlendMode.dstIn,
-                child: Obx(() {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Marquee(
-                        delay: const Duration(milliseconds: 300),
-                        duration: const Duration(seconds: 10),
-                        id: "${playerController.currentSong.value}_title",
-                        child: Text(
-                          playerController.currentSong.value?.title ?? "NA",
-                          style: Theme.of(context).textTheme.labelMedium!,
+        // Song Information (Title, Artist, Favorite, More)
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: ShaderMask(
+                  shaderCallback: (rect) {
+                    return const LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [Colors.white, Colors.white, Colors.transparent],
+                    ).createShader(
+                        Rect.fromLTWH(0, 0, rect.width, rect.height));
+                  },
+                  blendMode: BlendMode.dstIn,
+                  child: Obx(() {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Marquee(
+                          delay: const Duration(milliseconds: 300),
+                          duration: const Duration(seconds: 10),
+                          id: "${playerController.currentSong.value}_title",
+                          child: Text(
+                            playerController.currentSong.value?.title ?? "NA",
+                            style: Theme.of(context).textTheme.labelMedium!,
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 5),
-                      Marquee(
-                        delay: const Duration(milliseconds: 300),
-                        duration: const Duration(seconds: 10),
-                        id: "${playerController.currentSong.value}_subtitle",
-                        child: Text(
-                          playerController.currentSong.value?.artist ?? "NA",
-                          style: Theme.of(context).textTheme.labelSmall,
+                        const SizedBox(height: 5),
+                        Marquee(
+                          delay: const Duration(milliseconds: 300),
+                          duration: const Duration(seconds: 10),
+                          id: "${playerController.currentSong.value}_subtitle",
+                          child: Text(
+                            playerController.currentSong.value?.artist ?? "NA",
+                            style: Theme.of(context).textTheme.labelSmall,
+                          ),
                         ),
-                      )
-                    ],
-                  );
-                }),
-              ),
-            ),
-            // Favorite & More Button with Blur Background
-            Row(
-              children: [
-                _buildBlurredCircle(
-                  CupertinoButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: playerController.toggleFavourite,
-                    child: Obx(() => Icon(
-                          playerController.isCurrentSongFav.isFalse
-                              ? CupertinoIcons.heart
-                              : CupertinoIcons.heart_fill,
-                          color: Colors.white,
-                          size: 22,
-                        )),
-                  ),
+                      ],
+                    );
+                  }),
                 ),
-                const SizedBox(width: 5),
-                _buildBlurredCircle(
-                  CupertinoButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {
-                      showModalBottomSheet(
-                        constraints: const BoxConstraints(maxWidth: 500),
-                        shape: const RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.vertical(top: Radius.circular(10.0)),
-                        ),
-                        isScrollControlled: true,
-                        context: playerController
-                            .homeScaffoldkey.currentState!.context,
-                        barrierColor: Colors.transparent.withAlpha(100),
-                        builder: (context) => SongInfoBottomSheet(
-                          playerController.currentSong.value!,
-                          calledFromPlayer: true,
-                        ),
-                      ).whenComplete(() => Get.delete<SongInfoController>());
-                    },
-                    child: const Icon(
-                      CupertinoIcons.ellipsis,
-                      color: Colors.white,
-                      size: 22,
+              ),
+              Row(
+                children: [
+                  _buildBlurredCircle(
+                    CupertinoButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: playerController.toggleFavourite,
+                      child: Obx(() => Icon(
+                            playerController.isCurrentSongFav.isFalse
+                                ? CupertinoIcons.heart
+                                : CupertinoIcons.heart_fill,
+                            color: Colors.white,
+                            size: 22,
+                          )),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  const SizedBox(width: 5),
+                  _buildBlurredCircle(
+                    CupertinoButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {
+                        showModalBottomSheet(
+                          constraints: const BoxConstraints(maxWidth: 500),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(10.0)),
+                          ),
+                          isScrollControlled: true,
+                          context: playerController
+                              .homeScaffoldkey.currentState!.context,
+                          barrierColor: Colors.transparent.withAlpha(100),
+                          builder: (context) => SongInfoBottomSheet(
+                            playerController.currentSong.value!,
+                            calledFromPlayer: true,
+                          ),
+                        ).whenComplete(() => Get.delete<SongInfoController>());
+                      },
+                      child: const Icon(
+                        CupertinoIcons.ellipsis,
+                        color: Colors.white,
+                        size: 22,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
+
         const SizedBox(height: 20),
 
         // Progress Bar
-        GetX<PlayerController>(builder: (controller) {
-          return ProgressBar(
-            thumbRadius: 0,
-            barHeight: 4.5,
-            baseBarColor: Colors.grey[800],
-            bufferedBarColor: Colors.grey[600],
-            progressBarColor: Colors.grey[300],
-            thumbColor: Colors.transparent,
-            timeLabelTextStyle:
-                TextStyle(fontSize: 14, color: Colors.grey[300]),
-            progress: controller.progressBarStatus.value.current,
-            total: controller.progressBarStatus.value.total,
-            buffered: controller.progressBarStatus.value.buffered,
-            onSeek: controller.seek,
-          );
-        }),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              SliderTheme(
+                data: SliderTheme.of(context).copyWith(
+                  trackHeight: 6,
+                  activeTrackColor: Colors.grey[300],
+                  inactiveTrackColor: Colors.grey[800],
+                  thumbColor: Colors.grey[300],
+                  thumbShape:
+                      const RoundSliderThumbShape(enabledThumbRadius: 8),
+                  overlayShape: SliderComponentShape.noOverlay,
+                ),
+                child: Obx(
+                  () => Slider(
+                    min: 0,
+                    max: playerController
+                        .progressBarStatus.value.total.inSeconds
+                        .toDouble(),
+                    value: playerController
+                        .progressBarStatus.value.current.inSeconds
+                        .toDouble(),
+                    onChanged: (double newValue) {
+                      playerController
+                          .seek(Duration(seconds: newValue.toInt()));
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Obx(() => Text(
+                          _formatDuration(
+                              playerController.progressBarStatus.value.current),
+                          style:
+                              TextStyle(fontSize: 14, color: Colors.grey[300]),
+                        )),
+                    Obx(() => Text(
+                          _formatDuration(
+                              playerController.progressBarStatus.value.total),
+                          style:
+                              TextStyle(fontSize: 14, color: Colors.grey[300]),
+                        )),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+
         const SizedBox(height: 25),
 
-        // Player Controls: Previous, Play/Pause, Next
+        // Player Controls
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
@@ -162,46 +203,48 @@ class PlayerControlWidget extends StatelessWidget {
             ),
           ],
         ),
+
         const SizedBox(height: 10),
 
         // Volume Control
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               const Icon(CupertinoIcons.speaker_fill,
                   color: Colors.grey, size: 20),
               Expanded(
-                child: Obx(() {
-                  return SliderTheme(
-                    data: SliderTheme.of(context).copyWith(
-                      trackHeight: 4.5,
-                      activeTrackColor: Colors.grey[300],
-                      inactiveTrackColor: Colors.grey[800],
-                      thumbColor: Colors.transparent,
-                      thumbShape:
-                          const RoundSliderThumbShape(enabledThumbRadius: 0),
-                    ),
-                    child: CupertinoSlider(
+                child: SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    trackHeight: 6,
+                    activeTrackColor: Colors.grey[300],
+                    inactiveTrackColor: Colors.grey[800],
+                    thumbColor: Colors.grey[300],
+                    thumbShape:
+                        const RoundSliderThumbShape(enabledThumbRadius: 8),
+                    overlayShape: SliderComponentShape.noOverlay,
+                  ),
+                  child: Obx(() {
+                    return Slider(
                       min: 0,
                       max: 1,
                       value: playerController.volume.value,
                       onChanged: (double newValue) =>
                           playerController.setVolume(newValue),
-                      activeColor: Colors.grey[300],
-                    ),
-                  );
-                }),
+                    );
+                  }),
+                ),
               ),
               const Icon(CupertinoIcons.speaker_3_fill,
                   color: Colors.grey, size: 22),
             ],
           ),
         ),
-        const SizedBox(height: 10),
 
-        // Bottom Controls: Queue, Shuffle, Loop
+        const SizedBox(height: 20),
+
+        // Bottom Controls
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
@@ -242,18 +285,28 @@ class PlayerControlWidget extends StatelessWidget {
   // Helper method for blurred circular background
   Widget _buildBlurredCircle(Widget child) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
+      borderRadius: BorderRadius.circular(15),
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
         child: Container(
-          padding: const EdgeInsets.all(8),
+          width: 35,
+          height: 35,
+          padding: const EdgeInsets.all(6),
           decoration: BoxDecoration(
             color: Colors.grey.withOpacity(0.3),
             shape: BoxShape.circle,
           ),
-          child: child,
+          child: Center(child: child),
         ),
       ),
     );
+  }
+
+  // Utility function to format duration
+  String _formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
   }
 }
